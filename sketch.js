2a5285a7c395194e9de5d6db5d6d108c94fa6c2a
Juliana AIYEMOBOLA <juliana.aiyemobola@external.total.com>
2020-10-02 19:57:51 +0100

    added constraint

--- sketch.js
+++ sketch.js
@@ -33,9 +33,10 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(800,200,150,-PI/7);
 
     bird = new Bird(100,100);
-
+    constraint = new constraint(bird.body,log6.body);
 }
 
 function draw(){
@@ -58,7 +59,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
+    constraint.display();
     platform.display();
 }
